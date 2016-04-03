e0ca7711df9be89a229b41b6f62c35689abed1b7
abec8 <abec8@student.monash.edu>
2016-04-03 10:47:01 +1000

    Update MorseCodeReceiver.js

--- MorseCodeReceiver.js
+++ MorseCodeReceiver.js
@@ -24,6 +24,7 @@
  */
 
 // ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
+//var previousVal = "false", trueCounter = 0, falseCounter = 1,letterInMorse = "";
 
 /*
  * This function is called once per unit of time with camera image data.
@@ -37,7 +38,7 @@
  */
 function decodeCameraImage(data)
 {
-var redAmount = 0, blueAmount = 0, previousVal;
+var redAmount = 0, blueAmount = 0;
 
 //Need to call arrays referencing individual pixels. 
 for(i=0;i<(data.length)-1;i=i+4){
@@ -48,42 +49,70 @@ for(i=0;i<(data.length)-1;i=i+4){
         }
 }
     if (redAmount > blueAmount){
-        
-        /* if (previousVal == true){ //I assume we need a closure statement to preserve the value for previousVal beyond the local scope
-        previousValCounter += true //if there were true values before this one, it is added on
-        }
-        else{
-        previousValCounter = true //if the previous value was not true, we start a new counter
-        }
-        
-        */
+       //brace yourself, imma drop some dope ass knowledge here
+       /*
+       if (previousVal == true){ **this means we are in the process of determining a dot or a dash
+               trueCounter += 1;
+       }
+       else { **this means that we are starting to analyse a new character, hence need to start a new true count
+               **now need to analyse the kind of space coming before the new character
+               
+               if (falseCounter == 1 || falseCounter == 2){
+                       **interchar space
+                       **
+               }
+               elseif (falseCounter > 2 && falseCounter > 7){
+                        **interletter space
+                        **need to do something
+               }
+               else {
+                        **interword space
+                        **add space to output line
+               }
+               
+               trueCounter = 1;
+       }
+       previousVal = true;
+       */
         return true;
     }
     else {
+        /*
+        if (previousVal == false){ **this means we are determining the type of space
+                falseCounter += 1;
+        }
+        else { **this means we are starting to analyse a new space, hence need to start a new false count
+                if (trueCounter == 1 || trueCounter == 2){
+                        **need to add dot to letterInMorse
+                        letterInMorse += ".";
+                }
+                else {
+                        **need to add dash to letterInMorse
+                        letterInMorse += "-";
+                }
+                falseCounter = 1;
+        }
+        previousVal = false;
+        */
         return false;
-        //pastTwo = previousVal + false
-        //previousVal = false
+        
     }
     
 
  
  
  /* need to keep track of the previous value, counting the "true" values until a false
- **if there was 1 true, = "."
- **if there were 3 true, = "-"
- **if there is then 1 false, run again for new character
- **if there is then 3 false, parse through lookup table & run again for new letter
- **if there is then 7 false, = " " & run again for new word
+ **if there was 1||2 true, = "."
+ **if there were >3 true, = "-"
+ **if there is then 1||2 false, run again for new dot or dash
+ **if there is then >2&&<7 false, parse through lookup table & run again for new letter
+ **if there is then >6 false, = " " & run again for new word
  }
  
     this is how we define characters, which must come after the "decode image" function as the true & false statements are concatenated
     this output is then parsed through the lookup table by searching "lookupTable[<dot & dash arrangement>]" to determine what the letter is
  output += decodeCameraImage(data), though different characters have to be separated
-    
-    "."  = true
-    "-"  = true + true + true
-    letterSpace = false + false + false
-    wordSpace = false + false + false + false + false + false + false
+
  
     var lookupTable = {
     "." + “-” : "a",
