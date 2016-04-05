ce880991a4aafd6663c4fb51ea0e0ebb5603416e
abec8 <abec8@student.monash.edu>
2016-04-05 16:03:29 +1000

    Update MorseCodeReceiver.js

--- MorseCodeReceiver.js
+++ MorseCodeReceiver.js
@@ -24,7 +24,12 @@
  */
 
 // ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
-//var previousVal = "false", trueCounter = 0, falseCounter = 0,letterInMorse = "", output = "";
+var previousVal = "false",
+    trueCounter = 0,
+    falseCounter = 0,
+    letterInMorse = "",
+    output = "",
+    pixelTF;
 
 /*
  * This function is called once per unit of time with camera image data.
@@ -36,128 +41,123 @@
  * Output: You should return a boolean denoting whether or not the image is 
  *         an 'on' (red) signal.
  */
-function decodeCameraImage(data)
-{
-var redAmount = 0, blueAmount = 0;
+function decodeCameraImage(data) {
+    var redAmount = 0,
+        blueAmount = 0;
 
-//Need to call arrays referencing individual pixels. 
-for(i=0;i<(data.length)-1;i=i+4){
-        if (data[i] > data[i+2]){
-        redAmount += 1;
-        } else if (data[i] <= data[i+2]) {
-        blueAmount += 1;
+    //Need to call arrays referencing individual pixels. 
+    for (i = 0; i < (data.length) - 1; i = i + 4) {
+        if (data[i] > data[i + 2]) {
+            redAmount += 1;
+        } else if (data[i] <= data[i + 2]) {
+            blueAmount += 1;
         }
-}
-    if (redAmount > blueAmount){
-      return true;
-       if (previousVal == true){ //**this means we are in the process of determining a dot or a dash
-               trueCounter += 1;
-       }
-       else { //**this means that we are starting to analyse a new character, hence need to start a new true count
-               //**now need to analyse the kind of space coming before the new character
-               
-               if (falseCounter == 1 || falseCounter == 2){
-                       //**interelement space
-                       //**nothing happens
-               }
-               elseif (falseCounter > 2 && falseCounter < 7){
-                        //**intercharacter space
-                        output += lookupTable[letterInMorse];
-               }
-               else {
-                        //**interword space
-                        output += " ";
-               }
-               
-               trueCounter = 1;
-       }
-       previousVal = true;
     }
-    else {
+    if (redAmount > blueAmount) {
+        pixelTF = true;
+        return true;
+    } else {
+        pixelTF = false;
         return false;
-        if (previousVal == false){ //**this means we are determining the type of space
-                falseCounter += 1;
-        }
-        else { //**this means we are starting to analyse a new space, hence need to start a new false count
-                if (trueCounter == 1 || trueCounter == 2){
-                        //**need to add dot to letterInMorse
-                        letterInMorse += ".";
-                }
-                else {
-                        //**need to add dash to letterInMorse
-                        letterInMorse += "-";
-                }
-                falseCounter = 1;
-        }
-        previousVal = false;
     }
-    
 
- 
- 
+    if (pixelTF == true) {
 
- }
- 
-//    this is how we define characters, which must come after the "decode image" function as the true & false statements are concatenated
-//    this output is then parsed through the lookup table by searching "lookupTable[<dot & dash arrangement>]" to determine what the letter is
-// output += decodeCameraImage(data), though different characters have to be separated
+        if (previousVal == true) { //**this means we are in the process of determining a dot or a dash
+            trueCounter += 1;
+        } else { //**this means that we are starting to analyse a new character, hence need to start a new true count
+            //**now need to analyse the kind of space coming before the new character
 
- 
+            if (falseCounter == 1 || falseCounter == 2) {
+                //**interelement space
+                //**nothing happens
+            }
+            else if(falseCounter > 2 && falseCounter < 7) {
+                //**intercharacter space
+                output += lookupTable[letterInMorse];
+            } else {
+                //**interword space
+                output += " ";
+            }
+            trueCounter = 1;
+            previousVal = true;
+        }
+    } else {
+        if (previousVal == false) { //**this means we are determining the type of space
+            falseCounter += 1;
+        } else { //**this means we are starting to analyse a new space, hence need to start a new false count
+            if (trueCounter == 1 || trueCounter == 2) {
+                //**need to add dot to letterInMorse
+                letterInMorse += ".";
+            } else {
+                //**need to add dash to letterInMorse
+                letterInMorse += "-";
+            }
+            falseCounter = 1;
+            previousVal = false;
+        }
+    }
+
+    //    this is how we define characters, which must come after the "decode image" function as the true & false statements are concatenated
+    //    this output is then parsed through the lookup table by searching "lookupTable[<dot & dash arrangement>]" to determine what the letter is
+    // output += decodeCameraImage(data), though different characters have to be separated
+
+
     var lookupTable = {
-    .- : "a",
-    -... : "b",
-    -.-. : "c",
-    -.. : "d",
-    . : "e",
-    ..-. : "f",
-    --. : "g",
-    .... : "h",
-    .. : "i",
-    .--- : "j",
-    -.- : "k",
-    .-.. : "l",
-    -- : "m",
-    -. : "n",
-    --- : "o",
-    .--. : "p",
-    --.- : "q",
-    .-. : "r",
-    ... : "s",
-    - : "t",
-    ..- : "u",
-    ...- : "v",
-    .-- : "w",
-    -..- : "x",
-    -.-- : "y",
-    --.. : "z",
-    ----- : "0",
-    .---- : "1",
-    ..--- : "2",
-    ...-- : "3",
-    ....- : "4",
-    ..... : "5",
-    -.... : "6",
-    --... : "7",
-    ---.. : "8",
-    ----. : "9",
-    -.--. : "(",
-    -.--.- : ")",
-    .-..-. : """,
-    -...- : "=",
-    .----. : "'",
-    -..-. : "/",
-    .-.-. : "+",
-    ---... : ":",
-    .-.-.- : .,
-    --..-- : ",",
-    ..--.. : "?",
-    -....- : -,
-    .--.-. : "@",
-    ...-..- : "$",
-    ..--.- : "_",
-    -.-.-- : "!",
-    .-.- : " ",
-    ...-.- : messageFinished()
-};
-messageField.innerHTML = output;
+        ".-": "a", 
+        "-...": "b", 
+        "-.-.": "c", 
+        "-..": "d", 
+        ".": "e",
+        "..-.": "f",
+        "--.": "g",
+        "....": "h",
+        "..": "i",
+        ".---": "j", 
+        "-.-": "k",
+        ".-..": "l",
+        "--": "m", 
+        "-.": "n",
+        "---": "o",
+        ".--.": "p",
+        "--.-": "q",
+        ".-.": "r",
+        "...": "s", 
+        "-": "t",
+        "..-": "u",
+        "...-": "v",
+        ".--": "w", 
+        "-..-": "x", 
+        "-.--": "y",
+        "--..": "z",
+        "-----": "0",
+        ".----": "1",
+        "..---": "2",
+        "...--": "3",
+        "....-": "4",
+        ".....": "5", 
+        "-....": "6",
+        "--...": "7",
+        "---..": "8",
+        "----.": "9", 
+        "-.--.": "(", 
+        "-.--.-": ")",
+        ".-..-.": " '' ", 
+        "-...-": " = ",
+        ".----.": "'", 
+        "-..-.": "/",
+        ".-.-.": "+",
+        "---...": ":",
+        ".-.-.-": ".",
+        "--..--": ",",
+        "..--..": "?", 
+        "-....-": "-",
+        ".--.-.": "@",
+        "...-..-": "$",
+        "..--.-": "_", 
+        "-.-.--": "!",
+        ".-.-": " ",
+        "...-.-": messageFinished()
+    };
+    messageField.innerHTML = output;
 }
