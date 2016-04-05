31a938c601fd7c13544fc3980dc2f93eae381bf2
abec8 <abec8@student.monash.edu>
2016-04-05 14:55:46 +1000

    Update MorseCodeReceiver.js

--- MorseCodeReceiver.js
+++ MorseCodeReceiver.js
@@ -24,7 +24,7 @@
  */
 
 // ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
-//var previousVal = "false", trueCounter = 0, falseCounter = 0,letterInMorse = "";
+//var previousVal = "false", trueCounter = 0, falseCounter = 0,letterInMorse = "", output = "";
 
 /*
  * This function is called once per unit of time with camera image data.
@@ -44,13 +44,12 @@ var redAmount = 0, blueAmount = 0;
 for(i=0;i<(data.length)-1;i=i+4){
         if (data[i] > data[i+2]){
         redAmount += 1;
-        } else if (data[i] < data[i+2]) {
+        } else if (data[i] <= data[i+2]) {
         blueAmount += 1;
         }
 }
     if (redAmount > blueAmount){
-       //brace yourself, imma drop some dope ass knowledge here
-       /*
+      
        if (previousVal == true){ **this means we are in the process of determining a dot or a dash
                trueCounter += 1;
        }
@@ -59,27 +58,25 @@ for(i=0;i<(data.length)-1;i=i+4){
                
                if (falseCounter == 1 || falseCounter == 2){
                        **interelement space
-                       **letter ID stays unchanged
+                       **nothing happens
                }
-               elseif (falseCounter > 2 && falseCounter > 7){
+               elseif (falseCounter > 2 && falseCounter < 7){
                         **intercharacter space
-                        **previous letter ID needs to be analysed and saved
-                        **new letter ID is created 
+                        output += lookupTable[letterInMorse];
                }
                else {
                         **interword space
-                        **previous letter IDs are concatenated + a space
-                        **new letter ID is created
+                        output += " ";
                }
                
                trueCounter = 1;
        }
        previousVal = true;
-       */
+      
         return true;
     }
     else {
-        /*
+
         if (previousVal == false){ **this means we are determining the type of space
                 falseCounter += 1;
         }
@@ -95,7 +92,7 @@ for(i=0;i<(data.length)-1;i=i+4){
                 falseCounter = 1;
         }
         previousVal = false;
-        */
+        
         return false;
         
     }
@@ -103,76 +100,69 @@ for(i=0;i<(data.length)-1;i=i+4){
 
  
  
- /* need to keep track of the previous value, counting the "true" values until a false
- **if there was 1||2 true, = "."
- **if there were >3 true, = "-"
- **if there is then 1||2 false, run again for new dot or dash
- **if there is then >2&&<7 false, parse through lookup table & run again for new letter
- **if there is then >6 false, = " " & run again for new word
+
  }
  
-    this is how we define characters, which must come after the "decode image" function as the true & false statements are concatenated
-    this output is then parsed through the lookup table by searching "lookupTable[<dot & dash arrangement>]" to determine what the letter is
- output += decodeCameraImage(data), though different characters have to be separated
+//    this is how we define characters, which must come after the "decode image" function as the true & false statements are concatenated
+//    this output is then parsed through the lookup table by searching "lookupTable[<dot & dash arrangement>]" to determine what the letter is
+// output += decodeCameraImage(data), though different characters have to be separated
 
  
     var lookupTable = {
-    "." + “-” : "a",
-    “-” + "." + "." + "." : "b",
-    “-” + "." + “-” + "." : "c",
-    “-” + "." + "." : "d",
-    "." : "e",
-    "." + "." + “-” + "." : "f",
-    “-” + “-” + "." : "g",
-    "." + "." + "." + "." : "h",
-    "." + "." : "i",
-    "." + “-” + “-” + “-” : "j",
-    “-” + "." + “-” : "k",
-    “.” + “-” + “.” + “.” : "l",
-    “-” + “-” : "m",
-    “-” + “.” : "n",
-    “-” + “-” + “-” : "o",
-    “.” + “-” + “-” + “.” : "p",
-    “-” + “-” + “.” + “-” : "q",
-    “.” + “-” + “.” : "r",
-    “.” + “.” + “.” : "s",
-    “-” : "t",
-    “.” + “.” + “-” : "u",
-    “.” + “.” + “.” + “-” : "v",
-    “.” + “-” + “-” : "w",
-    “-” + “.” + “.” + “-” : "x",
-    “-” + “.” + “-” + “-” : "y",
-    “-” + “-” + “.” + “.” : "z",
-    “-” + “-” + “-” + “-” + “-” : "0",
-    “.” + “-” + “-” + “-” + “-” : "1",
-    “.” + “.” + “-” + “-” + “-” : "2",
-    “.” + “.” + “.” + “-” + “-” : "3",
-    “.” + “.” + “.” + “.” + “-” : "4",
-    “.” + “.” + “.” + “.” + “.” : "5",
-    “-” + “.” + “.” + “.” + “.” : "6",
-    “-” + “-” + “.” + “.” + “.” : "7",
-    “-” + “-” + “-” + “.” + “.” : "8",
-    “-” + “-” + “-” + “-” + “.” : "9",
-    “-” + “.” + “-” + “-” + “.” : "(",
-    “-” + “.” + “-” + “-” + “.” + “-” : ")",
-    “.” + “-” + “.” + “.” + “-” + “.” : """,
-    “-” + “.” + “.” + “.” + “-” : "=",
-    “.” + “-” + “-” + “-” + “-” + “.” : "'",
-    “-” + “.” + “.” + “-” + “.” : "/",
-    “.” + “-” + “.” + “-” + “.” : "+",
-    “-” + “-” + “-” + “.” + “.” + “.” : ":",
-    “.” + “-” + “.” + “-” + “.” + “-” : ".",
-    “-” + “-” + “.” + “.” + “-” + “-” : ",",
-    “.” + “.” + “-” + “-” + “.” + “.” : "?",
-    “-” + “.” + “.” + “.” + “.” + “-” : "-",
-    “.” + “-” + “-” + “.” + “-” + “.” : "@",
-    “.” + “.” + “.” + “-” + “.” + “.” + “-” : "$",
-    “.” + “.” + “-” + “-” + “.” + “-” : "_",
-    “-” + “.” + “-” + “.” + “-” + “-” : "!",
-    “.” + “-” + “.” + “-” : " ",
-    “.” + “.” + “.” + “-” + “.” + “-” : "<br/>"
-
-
+    .- : "a",
+    -... : "b",
+    -.-. : "c",
+    -.. : "d",
+    . : "e",
+    ..-. : "f",
+    --. : "g",
+    .... : "h",
+    .. : "i",
+    .--- : "j",
+    -.- : "k",
+    .-.. : "l",
+    -- : "m",
+    -. : "n",
+    --- : "o",
+    .--. : "p",
+    --.- : "q",
+    .-. : "r",
+    ... : "s",
+    - : "t",
+    ..- : "u",
+    ...- : "v",
+    .-- : "w",
+    -..- : "x",
+    -.-- : "y",
+    --.. : "z",
+    ----- : "0",
+    .---- : "1",
+    ..--- : "2",
+    ...-- : "3",
+    ....- : "4",
+    ..... : "5",
+    -.... : "6",
+    --... : "7",
+    ---.. : "8",
+    ----. : "9",
+    -.--. : "(",
+    -.--.- : ")",
+    .-..-. : """,
+    -...- : "=",
+    .----. : "'",
+    -..-. : "/",
+    .-.-. : "+",
+    ---... : ":",
+    .-.-.- : .,
+    --..-- : ",",
+    ..--.. : "?",
+    -....- : -,
+    .--.-. : "@",
+    ...-..- : "$",
+    ..--.- : "_",
+    -.-.-- : "!",
+    .-.- : " ",
+    ...-.- : messageFinished()
 };
-*/
+messageField.innerHTML = output;
 }
